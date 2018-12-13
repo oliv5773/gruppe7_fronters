0bc2ae39b19d5778ae92b1fe601f98ff63211544
Jesper Ersø Philipsen <jephilipsen@ejer.kea.local>
2018-12-13 10:30:50 +0100

    added blur

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function showHome() {
 
     document.querySelector("besked").classList.add("hide");
-    document.querySelector("besked").addEventListener("click", blurHome);
+    document.querySelector("besked").addEventListener("click", clickHome);
 }
 
-function blurHome() {
+function clickHome() {
 
 
-    document.querySelector("homecontainer").classList.add("blur);
-    }
+    document.querySelector("homecontainer").classList.add("blur");
+
+    document.querySelector("besked").addEventListener("click", showHome);
+
+}
